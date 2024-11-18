d9638cc36a2659fe32f3d3951e1fff1c50109164
Ori Ziskind <9935756+orizis@users.noreply.github.com>
2024-11-18 11:31:17 +0000

    Test commit

--- vim-canvas-demo-app-react/src/main.tsx
+++ vim-canvas-demo-app-react/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./globals.css";
 import { AppWrapper } from "./AppWrapper.tsx";
+import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <AppWrapper>

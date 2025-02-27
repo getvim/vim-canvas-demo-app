80c393f1885f9d8e22f1aad330b7391dd75f89f3
Anat <anatbard@getvim.com>
2025-02-27 12:03:46 +0000

    redirect to relevant app according to launchType

--- vim-canvas-demo-app-react/functions/api/launch.ts
+++ vim-canvas-demo-app-react/functions/api/launch.ts
@@ -1,9 +1,18 @@
 import { Env } from "../context-env";
 
+const SETTINGS_LAUNCH_TYPE = "APP_SETTINGS";
+
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   const url = new URL(context.request.url);
   const queryParams = url.searchParams;
   const launchId = queryParams.get("launch_id");
+  const launchType = queryParams.get("launch_type");
+
+  let redirect_uri = context.env.REDIRECT_URL ?? "http://localhost:8788";
+  redirect_uri =
+    launchType === SETTINGS_LAUNCH_TYPE
+      ? `${redirect_uri}/settings`
+      : redirect_uri;
 
   const redirectUrl = new URL(
     context.env.VIM_AUTHORIZE_ENDPOINT ??
@@ -11,10 +20,7 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
   );
   redirectUrl.searchParams.append("launch_id", launchId);
   redirectUrl.searchParams.append("client_id", context.env.CLIENT_ID);
-  redirectUrl.searchParams.append(
-    "redirect_uri",
-    context.env.REDIRECT_URL ?? "http://localhost:8788"
-  );
+  redirectUrl.searchParams.append("redirect_uri", redirect_uri);
   redirectUrl.searchParams.append("response_type", "code");
   return Response.redirect(redirectUrl.toString(), 302);
 };

52828afbaed199e20efce09f595b89022bde805f
LiorChervinsky <liorc@getvim.com>
2025-03-02 11:52:48 +0000

    SAM-66837 - Refactored Preview - Extracted ColorPickerModal Hook

--- vim-canvas-demo-app-react/src/components/settings-app/BrandCustomization.tsx
+++ vim-canvas-demo-app-react/src/components/settings-app/BrandCustomization.tsx
@@ -1,38 +1,8 @@
 import React, { useCallback, useState } from "react";
-import { ColorPicker } from "./ColorPicker";
-import { VimConnectPreview } from "./VimConnectPreview";
 import { useOrganizationContext } from "@/hooks/useOrganizationContext";
 import { saveSettings } from "../../utils/storage";
-
-interface BrandCustomizationFormProps {
-  appColor: string;
-  onColorChange: (color: string) => void;
-  onSave: () => void;
-}
-
-const BrandCustomizationForm: React.FC<BrandCustomizationFormProps> = ({
-  appColor,
-  onColorChange,
-  onSave,
-}) => {
-  return (
-    <div className="w-full a">
-      <div className="mb-8">
-        <label className="block text-lg font-medium mb-4">
-          Select your app color:
-        </label>
-        <ColorPicker color={appColor} onChange={onColorChange} />
-      </div>
-
-      <button
-        onClick={onSave}
-        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-medium py-3 px-12 rounded w-48"
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+import { VimConnectPreview } from "./VimConnectPreview";
+import { ColorPicker } from "./ColorPicker";
 
 export const BrandCustomization: React.FC = () => {
   const [appColor, setAppColor] = useState<string>("#00FFE1");
@@ -42,24 +12,34 @@ export const BrandCustomization: React.FC = () => {
     saveSettings({ [organizationId]: { appColor } });
   }, [organizationId, appColor]);
 
+
   return (
-    <div className="w-full max-w-6xl mx-auto p-[20px] font-proxima bg-white rounded-[10px]">
-      <div className="text-sm font-bold mb-8">
+    <div className="w-full p-5 font-proxima rounded-xl bg-white">
+      <div className="font-proxima text-sm leading-4 font-bold text-left mb-1">
         Customize Demo Canvas in-app experience to your organization branding
       </div>
 
-      <div className="flex flex-row gap-8">
-        <BrandCustomizationForm
-          appColor={appColor}
-          onColorChange={setAppColor}
-          onSave={handleSave}
-        />
+      <div className="grid grid-cols-[267px_9px_268px] gap-4 mt-5">
+        <div>
+          <div>
+            <label className="text-left text-sm font-bold">
+              Select your app color:
+            </label>
 
-        <div className="border-l-2 border-[#828282] self-stretch"></div>
+            <ColorPicker color={appColor} onChange={setAppColor} />
+          </div>
 
-        <div className="w-1/2 flex justify-center">
-          <VimConnectPreview appColor={appColor} />
+          <button
+            className="mt-5 bg-gray-300 hover:bg-gray-400 text-white text-center text-sm font-medium rounded-sm w-[100px] h-[34px]"
+            onClick={handleSave}
+          >
+            Save
+          </button>
         </div>
+
+        <div className="mx-3 border-l-[1px] border-[#828282]"></div>
+
+        <VimConnectPreview appColor={appColor} />
       </div>
     </div>
   );

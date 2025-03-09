0a42b7e1243c9caed174dfdeab8de807873fdd38
Anat Bardach <169037729+AnatBardach1@users.noreply.github.com>
2025-03-09 13:13:52 +0200

    add saved indication when changed color successful (#30)

--- vim-canvas-demo-app-react/src/components/settings-app/BrandCustomization.tsx
+++ vim-canvas-demo-app-react/src/components/settings-app/BrandCustomization.tsx
@@ -8,17 +8,29 @@ export const BrandCustomization: React.FC = () => {
   const [appColor, setAppColor] = useState<string>("#00FFE1");
   const { idToken } = useAuthTokenData();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [saveButtonText, setSaveButtonText] = useState<string>("Save");
   const [isSaveButtonDisabled, setIsSaveButtonDisabled] =
     useState<boolean>(true);
 
   const handleSave = useCallback(() => {
-    saveSettings(
-      {
-        theme_color: appColor,
-      },
-      idToken
-    );
-    setIsSaveButtonDisabled(true);
+    const save = async () => {
+      try {
+        await saveSettings(
+          {
+            theme_color: appColor,
+          },
+          idToken
+        );
+        setTimeout(() => {
+          setSaveButtonText("Save");
+        }, 2000);
+        setIsSaveButtonDisabled(true);
+        setSaveButtonText("Saved!");
+      } catch (error) {
+        console.error("Failed to save settings", error);
+      }
+    };
+    save();
   }, [appColor, idToken]);
 
   useEffect(() => {
@@ -70,7 +82,7 @@ export const BrandCustomization: React.FC = () => {
               backgroundColor: isSaveButtonDisabled ? "#D1D5DB" : "#001C36",
             }}
           >
-            Save
+            {saveButtonText}
           </button>
         </div>
 

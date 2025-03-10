d4cb46921f24db60c5d21d6b2d8cf9b2f761bb94
LiorChervinsky <liorc@getvim.com>
2025-03-10 08:38:17 +0000

    SAM-66238--FE--APP--AI-SCRIBE - PR fixes

--- vim--ai-scribe-app/functions/api/launch.ts
+++ vim--ai-scribe-app/functions/api/launch.ts
@@ -1,21 +1,14 @@
 import { Env } from "../context-env";
 
-const SETTINGS_LAUNCH_TYPE = "APP_SETTINGS";
-
 /** NOTE: Route is `/api/launch` */
 export const onRequestGet: PagesFunction<Env> = async (context) => {
 
   const url = new URL(context.request.url);
   const queryParams = url.searchParams;
   const launchId = queryParams.get("launch_id");
-  const launchType = queryParams.get("launch_type");
 
   // Redirect to the settings application page if the launch type is APP_SETTINGS
-  let redirect_uri = context.env.REDIRECT_URL ?? "http://localhost:8788";
-  redirect_uri =
-    launchType === SETTINGS_LAUNCH_TYPE
-      ? `${redirect_uri}/settings`
-      : redirect_uri;
+  const redirect_uri = context.env.REDIRECT_URL ?? "http://localhost:8788";
 
   const redirectUrl = new URL(
     context.env.VIM_AUTHORIZE_ENDPOINT ??
@@ -25,5 +18,6 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
   redirectUrl.searchParams.append("client_id", context.env.CLIENT_ID);
   redirectUrl.searchParams.append("redirect_uri", redirect_uri);
   redirectUrl.searchParams.append("response_type", "code");
+
   return Response.redirect(redirectUrl.toString(), 302);
 };
--- vim--ai-scribe-app/src/main.tsx
+++ vim--ai-scribe-app/src/main.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppContextProviders } from "./providers/AppContextProviders.tsx";
-import { AppSettingsContextProviders } from "./providers/AppSettingsContextProviders.tsx";
 import { App } from "./App.tsx";
-import { AppSettings } from "./AppSettings.tsx";
 import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -15,14 +13,6 @@ createRoot(document.getElementById("root")!).render(
           <AppContextProviders>
             <App />
           </AppContextProviders>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <AppSettingsContextProviders>
-            <AppSettings />
-          </AppSettingsContextProviders>
         }
       />
     </Routes>

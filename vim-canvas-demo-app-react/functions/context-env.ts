71441fa1c6c01022dcb41f75d6a351c4ffdf69d6
ed <cruz@beamdata.xyz>
2025-03-11 11:22:09 -0700

    fix: add missing environment variables to Env interface

--- vim-canvas-demo-app-react/functions/context-env.ts
+++ vim-canvas-demo-app-react/functions/context-env.ts
@@ -4,6 +4,10 @@ export interface Env {
   CLIENT_SECRET: string;
   REDIRECT_URL: string;
   SCRIBEAI_API_KEY?: string;
+  VIM_TOKEN_ENDPOINT?: string;
+  VIM_AUTHORIZE_ENDPOINT?: string;
+  VIM_ISSUER?: string;
+  CLIENT_SECRET_FALLBACK?: string;
 }
 
 // Export environment variables for use in functions
@@ -11,5 +15,9 @@ export const getEnv = (env: Env) => ({
   CLIENT_ID: env.CLIENT_ID,
   CLIENT_SECRET: env.CLIENT_SECRET,
   REDIRECT_URL: env.REDIRECT_URL || 'http://localhost:8788',
-  SCRIBEAI_API_KEY: env.SCRIBEAI_API_KEY
+  SCRIBEAI_API_KEY: env.SCRIBEAI_API_KEY,
+  VIM_TOKEN_ENDPOINT: env.VIM_TOKEN_ENDPOINT,
+  VIM_AUTHORIZE_ENDPOINT: env.VIM_AUTHORIZE_ENDPOINT,
+  VIM_ISSUER: env.VIM_ISSUER,
+  CLIENT_SECRET_FALLBACK: env.CLIENT_SECRET_FALLBACK
 });

38d01c8069ce802f9547631680ed9fdd1ac8365c
vimHopemanRyan <ryanho@getvim.com>
2025-03-12 14:22:55 +0000

    when element is scrolled to it will pop up header to be sticky header

--- vim-canvas-demo-app-react/src/components/encounter-content/index.tsx
+++ vim-canvas-demo-app-react/src/components/encounter-content/index.tsx
@@ -23,21 +23,24 @@ import { EncounterPI } from "./PatientInstructions";
 import { EncounterPlan } from "./Plan";
 import { EncounterSubjective } from "./Subjective";
 import { EncounterBillingInformation } from "./BillingInformation";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useState,
+} from "react";
+
+const scrollY = 530;
 
 export const EncounterContent = () => {
   const { toast } = useToast();
   const { jsonMode } = useAppConfig();
   const { encounter } = useVimOSEncounter();
   const { canUpdate, updateEncounter } = useUpdateEncounter();
-
+  const [enlargedHeader, setEnlargeHeader] = useState(false);
   const methods = useNotesForm();
 
-  /**
-   * Because input fields are never disabled (because when they can't be updated we show copy to clipboard UX),
-   * We can't use react-hook-form to determine which inputs are for copy to clipboard and which are for updating.
-   *
-   * So we have to manually check if the input fields are dirty and if they can be updated.
-   */
   const canUpdateObj: EHR.CanUpdateEncounterParams = {
     assessment: {},
     objective: {},
@@ -82,125 +85,107 @@ export const EncounterContent = () => {
   const canUpdateResult = canUpdate(canUpdateObj);
   const areNotesDirty = Object.keys(methods.formState.dirtyFields).length > 0;
 
-  const canUpdateNotes = {
-    ...canUpdateResult,
-    canUpdate: areNotesDirty && canUpdateResult.canUpdate,
-  };
+  const canUpdateNotes = useMemo(
+    () => ({
+      ...canUpdateResult,
+      canUpdate: areNotesDirty && canUpdateResult.canUpdate,
+    }),
+    [areNotesDirty, canUpdateResult]
+  );
 
-  const onNotesSubmit = async (data: FormInputs) => {
-    function removeUndefinedProperties(obj: unknown) {
-      if (typeof obj !== "object" || obj === null) {
-        return obj;
-      }
+  const onNotesSubmit = useCallback(
+    async (data: FormInputs) => {
+      const encounterPayload: EHR.UpdateEncounterParams = {
+        subjective: {
+          generalNotes: canUpdateNotes?.details.subjective?.generalNotes
+            ? data.subjectiveGeneralNotes ?? undefined
+            : undefined,
+          chiefComplaintNotes: canUpdateNotes?.details.subjective
+            ?.chiefComplaintNotes
+            ? data.subjectiveChiefComplaint ?? undefined
+            : undefined,
+          historyOfPresentIllnessNotes: canUpdateNotes?.details.subjective
+            ?.historyOfPresentIllnessNotes
+            ? data.subjectiveHistoryOfPresentIllness ?? undefined
+            : undefined,
+          reviewOfSystemsNotes: canUpdateNotes?.details.subjective
+            ?.reviewOfSystemsNotes
+            ? data.subjectiveReviewOfSystems ?? undefined
+            : undefined,
+        },
+        objective: {
+          generalNotes: canUpdateNotes?.details.objective?.generalNotes
+            ? data.objectiveGeneralNotes ?? undefined
+            : undefined,
+          physicalExamNotes: canUpdateNotes?.details.objective
+            ?.physicalExamNotes
+            ? data.objectivePhysicalExamNotes ?? undefined
+            : undefined,
+        },
+        assessment: {
+          generalNotes: canUpdateNotes?.details.assessment?.generalNotes
+            ? data.assessmentGeneralNotes ?? undefined
+            : undefined,
+        },
+        plan: {
+          generalNotes: canUpdateNotes?.details.plan?.generalNotes
+            ? data.planGeneralNotes ?? undefined
+            : undefined,
+        },
+        patientInstructions: {
+          generalNotes: canUpdateNotes?.details.patientInstructions
+            ?.generalNotes
+            ? data.patientInstructionsGeneralNotes ?? undefined
+            : undefined,
+        },
+      };
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const result: any = {};
+      updateEncounter(encounterPayload)
+        .then(() => {
+          toast({
+            variant: "default",
+            title: "Encounter notes updated!",
+          });
+        })
+        .catch((error) => {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: error ? JSON.stringify(error) : "An error occurred.",
+          });
+        });
+      methods.reset();
+    },
+    [canUpdateNotes, methods, toast, updateEncounter]
+  );
 
-      for (const [key, value] of Object.entries(obj)) {
-        if (typeof value === "object" && value !== null) {
-          const nestedResult = removeUndefinedProperties(value);
-          if (Object.keys(nestedResult).length > 0) {
-            result[key] = nestedResult;
-          }
-        } else if (value !== undefined) {
-          result[key] = value;
-        }
-      }
+  const headerClasses = useMemo(() => {
+    return `flex justify-between items-center top-0 z-10 bg-white  p-2  duration-2 ${
+      enlargedHeader
+        ? " animate-[fadeIn_0.2s_forwards] opacity-0 pr-6 pl-4 fixed top-0  left-0 pt-0  w-full shadow-md"
+        : ""
+    }`;
+  }, [enlargedHeader]);
 
-      return result;
-    }
-    const encounterPayload: EHR.UpdateEncounterParams = {
-      subjective: {
-        generalNotes: canUpdateNotes?.details.subjective?.generalNotes
-          ? data.subjectiveGeneralNotes ?? undefined
-          : undefined,
-        chiefComplaintNotes: canUpdateNotes?.details.subjective
-          ?.chiefComplaintNotes
-          ? data.subjectiveChiefComplaint ?? undefined
-          : undefined,
-        historyOfPresentIllnessNotes: canUpdateNotes?.details.subjective
-          ?.historyOfPresentIllnessNotes
-          ? data.subjectiveHistoryOfPresentIllness ?? undefined
-          : undefined,
-        reviewOfSystemsNotes: canUpdateNotes?.details.subjective
-          ?.reviewOfSystemsNotes
-          ? data.subjectiveReviewOfSystems ?? undefined
-          : undefined,
-      },
-      objective: {
-        generalNotes: canUpdateNotes?.details.objective?.generalNotes
-          ? data.objectiveGeneralNotes ?? undefined
-          : undefined,
-        physicalExamNotes: canUpdateNotes?.details.objective?.physicalExamNotes
-          ? data.objectivePhysicalExamNotes ?? undefined
-          : undefined,
-      },
-      assessment: {
-        generalNotes: canUpdateNotes?.details.assessment?.generalNotes
-          ? data.assessmentGeneralNotes ?? undefined
-          : undefined,
-      },
-      plan: {
-        generalNotes: canUpdateNotes?.details.plan?.generalNotes
-          ? data.planGeneralNotes ?? undefined
-          : undefined,
-      },
-      patientInstructions: {
-        generalNotes: canUpdateNotes?.details.patientInstructions?.generalNotes
-          ? data.patientInstructionsGeneralNotes ?? undefined
-          : undefined,
-      },
+  const checkIfNeedToSetStickyHeader = useCallback(() => {
+    setEnlargeHeader(window.scrollY > scrollY);
+  }, [setEnlargeHeader]);
+
+  const onScroll = useCallback(() => {
+    checkIfNeedToSetStickyHeader();
+  }, [checkIfNeedToSetStickyHeader]);
+
+  useEffect(() => {
+    checkIfNeedToSetStickyHeader();
+    return () => {
+      setEnlargeHeader(false);
     };
+  }, [checkIfNeedToSetStickyHeader, setEnlargeHeader]);
 
-    updateEncounter(removeUndefinedProperties(encounterPayload))
-      .then(() => {
-        toast({
-          variant: "default",
-          title: "Encounter notes updated!",
-        });
-      })
-      .catch((error) => {
-        toast({
-          variant: "destructive",
-          title: "Uh oh! Something went wrong.",
-          description: error ? JSON.stringify(error) : "An error occurred.",
-        });
-      });
-    methods.reset({
-      subjectiveGeneralNotes: canUpdateNotes?.details.subjective?.generalNotes
-        ? null
-        : undefined,
-      subjectiveChiefComplaint: canUpdateNotes?.details.subjective
-        ?.chiefComplaintNotes
-        ? null
-        : undefined,
-      subjectiveHistoryOfPresentIllness: canUpdateNotes?.details.subjective
-        ?.historyOfPresentIllnessNotes
-        ? null
-        : undefined,
-      subjectiveReviewOfSystems: canUpdateNotes?.details.subjective
-        ?.reviewOfSystemsNotes
-        ? null
-        : undefined,
-      objectiveGeneralNotes: canUpdateNotes?.details.objective?.generalNotes
-        ? null
-        : undefined,
-      objectivePhysicalExamNotes: canUpdateNotes?.details.objective
-        ?.physicalExamNotes
-        ? null
-        : undefined,
-      assessmentGeneralNotes: canUpdateNotes?.details.assessment?.generalNotes
-        ? null
-        : undefined,
-      planGeneralNotes: canUpdateNotes?.details.plan?.generalNotes
-        ? null
-        : undefined,
-      patientInstructionsGeneralNotes: canUpdateNotes?.details
-        .patientInstructions?.generalNotes
-        ? null
-        : undefined,
-    });
-  };
+  useLayoutEffect(() => {
+    document.addEventListener("scroll", onScroll);
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [onScroll]);
 
   return (
     <div className="w-full">
@@ -220,17 +205,16 @@ export const EncounterContent = () => {
           <Separator className="mb-1" />
           <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(onNotesSubmit)}>
-              <div className="flex justify-between items-center">
-                <EntitySectionTitle
-                  className="text-md"
-                  title="Clinical notes"
-                />
+              <div className={headerClasses}>
+                <EntitySectionTitle className="text-md" title="SOAP" />
                 <Button
                   size="sm"
                   variant="default"
                   className="pl-2 pr-3 h-8"
                   disabled={!canUpdateNotes?.canUpdate}
-                  onClick={() => {}}
+                  onClick={() => {
+                    methods.handleSubmit(onNotesSubmit)();
+                  }}
                 >
                   <CheckIcon className="mr-2" />
                   Push all notes

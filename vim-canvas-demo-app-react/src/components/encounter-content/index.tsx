e52b50857b434715dfb4defa90fe71febba18622
vikaptash <72451893+vikaptash@users.noreply.github.com>
2025-08-06 12:39:34 +0300

    add encounter general notes (#63)
    Co-authored-by: Viktoriia Ptashynska <viktoriiaptashynska@Viktoriias-MacBook-Pro.local>

--- vim-canvas-demo-app-react/src/components/encounter-content/index.tsx
+++ vim-canvas-demo-app-react/src/components/encounter-content/index.tsx
@@ -22,6 +22,7 @@ import { EncounterPI } from "./PatientInstructions";
 import { EncounterPlan } from "./Plan";
 import { EncounterSubjective } from "./Subjective";
 import { EncounterBillingInformation } from "./BillingInformation";
+import { EncounterGeneralNotes } from "./EncounterNotes";
 import {
   useCallback,
   useEffect,
@@ -67,6 +68,7 @@ export const EncounterContent = () => {
       plan: {},
       subjective: {},
       billingInformation: {},
+      encounterNotes: {},
     }),
     []
   );
@@ -199,6 +201,8 @@ export const EncounterContent = () => {
               <EncounterPI />
               <Separator className="mb-1" />
               <EncounterBillingInformation />
+              <Separator className="mb-1" />
+              <EncounterGeneralNotes />
             </form>
           </FormProvider>
           <Separator className="mb-1" />
